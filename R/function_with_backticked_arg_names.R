4d7708dd895e361fe257c0d42cb8c4ddb1972448
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-09-19 14:30:50 -0400

    broke `document()`

---
+++ R/function_with_backticked_arg_names.R
@@ -0,0 +1,19 @@
+#' A function with argument names defined using backticks
+#'
+#' More information about this function.
+#'
+#' @param `arg 1` a number
+#' @param `arg 2` a string
+#'
+#' @return the string `"goodbye"`
+#' @export
+#'
+#' @examples
+function_with_backticked_arg_names = function(
+    `arg 1` = 1,
+    `arg 2` = "apple")
+{
+  print(`arg 1`)
+  print(`arg 2`)
+  return('goodbye')
+}
